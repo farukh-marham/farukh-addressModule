19471220c0defeb2c51386e4f6f249bb09c0bd3a
Farukh t <farukhtariq3@gmail.com>
2020-10-16 01:32:28 +0500

    Code

---
+++ app/src/main/java/com/example/myapplication/CustomViews/BodyText.java
@@ -0,0 +1,56 @@
+package com.example.myapplication.CustomViews;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.example.myapplication.Listeners.OnEnableListener;
+import com.example.myapplication.R;
+
+public class BodyText extends AppCompatTextView {
+
+    private OnEnableListener enableListner;
+    private String fontName;
+
+    public BodyText(Context context, boolean isBold) {
+        super(context);
+
+        if (!isInEditMode()) {
+            setFont(context, isBold);
+        }
+
+    }
+
+    public BodyText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        if (enableListner != null) {
+            enableListner.onEnable(enabled);
+        }
+    }
+
+
+    public void setOnEnableListener(OnEnableListener enableListner) {
+        this.enableListner = enableListner;
+    }
+
+    public void setFont(Context context, boolean isBold) {
+
+        if (fontName == null || fontName.length() == 0) {
+            fontName = context.getString(R.string.body_text);
+        }
+        Typeface tf = Typeface.createFromAsset(context.getAssets(), "fonts/Roboto-Regular.ttf");
+        if (isBold) {
+            setTypeface(tf, Typeface.BOLD);
+        } else {
+            setTypeface(tf);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Listeners/OnEnableListener.java
@@ -0,0 +1,5 @@
+package com.example.myapplication.Listeners;
+
+public interface OnEnableListener {
+    void onEnable(boolean value);
+}
